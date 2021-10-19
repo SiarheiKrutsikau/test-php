39e62432ce8ea18e9b72117dea6c6504914d6067
SiarheiKrutsikau <ernestiis@yahoo.com>
2021-10-19 23:00:19 +0300

    изменение ответа тестового задания 1

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 echo "Решение задачь в самом тексте PHP файла. Задача 1 и задача 2"; 
-exit(); // причина не правильный запрос к API
+
 //задача 2  в конце
 
-/* **Задача1: написать функцию, которая будет добавлять заказы в эту таблицу.***/
-/* Добавляются заказы с помощью MSQL запроса после проверок*/
+/* **Задача1: написать функцию, которая будет добавлять заказы в  таблицу.***/
+/* Добавляются заказы с помощью MySQL запроса после проверок*/
 
-// функция для API (api.site.com) броня предположительно
-function apisitecom($event_id2, $event_date2, $ticket_adult_price2, $ticket_adult_quantity2, $ticket_kid_price2,$ticket_kid_quantity2, $barcode2) {
+// функция для API (api.site.com) броня
+function apisitecom($event_id2, $event_date2, $ticket_adult_price2, $ticket_adult_quantity2, $ticket_kid_price2,$ticket_kid_quantity2, $barcode) {
   $query_data = array(
     'client' => 'x',
     'q' => $event_id2,
@@ -16,13 +16,12 @@ function apisitecom($event_id2, $event_date2, $ticket_adult_price2, $ticket_adul
     'sl2' => $ticket_adult_quantity2,
     'sl3' => $ticket_kid_price2,
     'sl4' => $ticket_kid_quantity2,
-    'tl' => $barcode2
+    'tl' => $barcode
   );
   $filename = 'https://api.site.com/book';
   $options = array(
     'http' => array(
-     // 'user_agent' => 'Mozilla/5.0 (Windows NT 6.0; rv:26.0) Gecko/20100101 Firefox/26.0',
-        'user_agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.67',
+      'user_agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.67',
       'method' => 'POST',
       'header' => 'Content-type: application/x-www-form-urlencoded',
       'content' => http_build_query($query_data)
@@ -34,29 +33,29 @@ function apisitecom($event_id2, $event_date2, $ticket_adult_price2, $ticket_adul
   return json_decode($response);
 }
 
+
 // функция для API запрос с подтверждением (https://api.site.com/approve)  
 // мало опыта  с API, предположительно
-function apisitecomapprove($barcode3) {
+function apisitecomapprove($barcode) {
   $query_data = array(
     'client' => 'x',
-    'tl' => $barcode3
+    'tl' => $barcode
   );
-  $filename1 = 'https://api.site.com/approve';
-  $options1= array(
-    'http' => array(
-     // 'user_agent' => 'Mozilla/5.0 (Windows NT 6.0; rv:26.0) Gecko/20100101 Firefox/26.0',
-        'user_agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.67',
+  $filename = 'https://api.site.com/approve';
+    $options = array(
+        'http' => array(
+      'user_agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.67',
       'method' => 'POST',
       'header' => 'Content-type: application/x-www-form-urlencoded',
       'content' => http_build_query($query_data)
     )
   );
-  $context1 = stream_context_create($options1);
-  $response1 = file_get_contents($filename1, false, $context1);
-  
-  return json_decode($response1);
+  $context = stream_context_create($options);
+    $response = file_get_contents($filename, false, $context);
+
+    return json_decode($response);
 }
-//допустим есть аргументы
+
 $event_id;
 $event_date;
 $ticket_adult_price;
@@ -64,7 +63,7 @@ $ticket_adult_quantity;
 $ticket_kid_price;
 $ticket_kid_quantity;
 
-//преобразуем их к числовому виду (itn например)
+//преобразуем их к числовому виду (int)
 $event_id1=(int) $event_id;
 $event_date1=(int)$event_date;
 $ticket_adult_price1=(int)$ticket_adult_price;
@@ -72,18 +71,18 @@ $ticket_adult_quantity1=(int)$ticket_adult_quantity;
 $ticket_kid_price1=(int)$ticket_kid_price;
 $ticket_kid_quantity1=(int)$ticket_kid_quantity;
 
-//сгенерировать `barcode` c помощью md5
+//сгенерировать `barcode` c помощью случайного числа переведенного в md5
 //максимальная сумма (для уникальности)
 $rand_max=$event_id1+$event_date1+$ticket_adult_price1+$ticket_adult_quantity1+$ticket_kid_price1+$ticket_kid_quantity1;
-//цикл пока не придет подтверждение от API (api.site.com) броня
 
 do 
 {
-//случайное число
+//сгенерируем случайное число для получения barcode, который не должен повторяться
 $rand=rand(0,$rand_max);
-//barcode в виде md5
+//переведем случайное число в md5, для получения barcode
 $barcode= md5($rand);
-//отправка api через функцию предполагаю что так запрос составляется
+
+//отправка api через функцию apisitecom
 $tr=apisitecom($event_id, $event_date, $ticket_adult_price, $ticket_adult_quantity, $ticket_kid_price, $ticket_kid_quantity,$barcode);
 }
 while ($tr!='order successfully booked');
@@ -94,6 +93,7 @@ do
     $tr1=apisitecomapprove($barcode);
 }
 while ($tr1!='order successfully aproved');
+
 // подключение к базе данных и запись
 if ($tr1=='order successfully aproved')
 {
@@ -102,10 +102,10 @@ if ($tr1=='order successfully aproved')
     $password="12345";
     $database="base";
     ini_set('display_errors', 0);
-    $conect=mysqli_connect($host, $user, $password, $database); //подключаемся к серверу
+    //подключаемся к серверу
+    $connect = mysqli_connect($host, $user, $password, $database);
 
-    if(!$conect)
-        {
+    if ($connect == false) {
         echo "<div style='color:red'>Не возможно соединиться с базой данных. Подключите базу ...  </div>" ; 
         }
     else
@@ -113,22 +113,22 @@ if ($tr1=='order successfully aproved')
         ini_set('display_errors', 1);
         //echo "К базе подключено успешно"; 
         }
-  //общая сумма заказов
+
+    //общая сумма заказов
   $equal_price = $ticket_adult_price*$ticket_adult_quantity+$ticket_kid_price*$ticket_kid_quantity;
   //дата заказа
-  if (!$created)
-  {
+  if ($created == false) {
       $created=date('d-m-Y H:i:s');
   }
  // запись в БД
    do
    {
     $team="INSERT INTO table (`event_id`,`event_date`, `ticket_adult_price`, `ticket_adult_quantity`,`ticket_kid_price`, `ticket_kid_quantity`, `barcode`, `equal_price`, `created`) VALUES ($event_id, $event_date, $ticket_adult_price, $ticket_adult_quantity, $ticket_kid_price, $ticket_kid_quantity, $barcode, $equal_price, $created)";	
-mysqli_query($conect, $team);
-   }
+    mysqli_query($connect, $team);
+    }
    while(!$ok);
  // закрытие соединения с БД
-   mysql_close($conect);
+   mysql_close($connect);
 }
 
 /*### Задание №2 А ###
